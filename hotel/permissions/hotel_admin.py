0a96762e844ff2dc4e7b14e7e9505d1743c6a371
Ammar Zarzour <azar41550@gmail.com>
2024-11-17 08:45:29 +0300

    add hotel admin permissions

---
+++ hotel/permissions/hotel_admin.py
@@ -0,0 +1,12 @@
+from rest_framework.permissions import BasePermission
+from hotel.models import Hotel
+
+
+class HotelAdminPermission(BasePermission):
+    def has_permission(self, request, view):
+        hotel_id = view.kwargs.get("hotel_pk")
+        try:
+            hotel = Hotel.objects.only("hotel_super_admin").get(id=hotel_id)
+        except Hotel.DoesNotExist:
+            return False
+        return hotel and hotel.hotel_super_admin_id == request.user.id
