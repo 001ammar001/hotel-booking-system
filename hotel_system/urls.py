6d5a49a91a3094fc5e39c05508a2dfb73617913c
Ammar Zarzour <azar41550@gmail.com>
2024-11-17 08:47:28 +0300

    add media settings

--- hotel_system/urls.py
+++ hotel_system/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
     path("", include("accounts.urls")),
     path("", include("hotel.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
